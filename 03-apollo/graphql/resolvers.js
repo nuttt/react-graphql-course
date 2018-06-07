2e6a93b261a8fee4a6c8c19518bf7ad9a4da4e1c
Nuttapon Pattanavijit <nut@jitta.com>
2018-06-07 22:23:43 +0700

    Complete basic query

--- 03-apollo/graphql/resolvers.js
+++ 03-apollo/graphql/resolvers.js
@@ -2,15 +2,49 @@
 
 module.exports = {
   Query: {
+    posts: (obj, args, { models: { Post } }) => {
+      return Post.find()
+    },
     post: (obj, { id }, context) => {
       const Post = context.models.Post
       return Post.findById(id)
+    },
+    tags: (obj, args, { models: { Post }}) => {
+      return Post.distinct('tags')
+    },
+    tag: (obj, { name }) => name,
+    user: (obj, { id, username }, { models: { User }}) => {
+      if (username) {
+        return User.findOne({ username })
+      } else if (id) {
+        return User.findById(id)
+      } else {
+        throw new Error('must provide username or id')
+      }
     }
   },
   Post: {
-   author: (post, args, context) => {}
+    author: (post, args, { models: { User } }) => {
+      return User.findById(post.authorId)
+    },
+    comments: (post, args, { models: { Comment }}) => {
+      return Comment.find({ postId: post._id })
+    }
+  },
+  Comment: {
+    author: (post, args, { models: { User } }) => {
+      return User.findById(post.authorId)
+    }
+  },
+  User: {
+    posts: (user, args, { models: { Post } }) => {
+      return Post.find({ authorId: user._id })
+    }
   },
   Tag: {
-    name: (tagString) => {}
+    name: (tagString) => tagString,
+    posts: (tagString, args, { models: { Post } }) => {
+      return Post.find({ tags: req.params.tag })
+    }
   }
 }
